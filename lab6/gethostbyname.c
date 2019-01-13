be5f803773611123c873f6c7e05fe6cbf28ddad6
mengning <mengning@ustc.edu.cn>
2019-01-13 15:27:23 +0800

    Update gethostbyname.c

--- lab6/gethostbyname.c
+++ lab6/gethostbyname.c
@@ -1,24 +1,32 @@
+/* for inet_ntoa() */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include <netdb.h>
-#include <stdio.h>
+
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Usage: %s hostname", argv[0]);
-        exit(-1);
-    }
+ if (argc < 2) {
+   fprintf(stderr,"Usage: %s hostname\n", argv[0]);
+   exit(1);
+ }
 
-    struct hostent *hp = gethostbyname(argv[1]);
+ struct hostent *hp = gethostbyname(argv[1]);
 
-    if (hp == NULL) {
-       printf("gethostbyname() failed\n");
-    } else {
-       printf("%s = ", hp->h_name);
-       unsigned int i=0;
-       while ( hp -> h_addr_list[i] != NULL) {
-          printf( "%s ", inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[i])));
-          i++;
-       }
-       printf("\n");
-    }
+ if (hp == NULL) {
+   fprintf(stderr,"gethostbyname() failed\n");
+   exit(1);
+ } else {
+   printf("%s = ", hp->h_name);
+   unsigned int i=0;
+   while ( hp -> h_addr_list[i] != NULL) {
+     printf( "%s ", inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[i])));
+     i++;
+   }
+   printf("\n");
+   exit(0);
+ }
 }
